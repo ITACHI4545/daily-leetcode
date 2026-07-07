bf01671911d66d0daae915c6648100c3e75ceb84
ITACHI4545 <singhabhitendrapratap@gmail.com>
2026-07-07 08:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (85.34%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    private void helper(TreeNode root,List<Integer> ans){
+        if(root==null) return;
+        ans.add(root.val);
+        helper(root.left,ans);
+        helper(root.right,ans);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root,ans);
+        return ans;
+    }
+}
