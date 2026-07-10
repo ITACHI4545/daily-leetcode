fab1304234c7fdb68d0cf3cb90e922560ce13d93
ITACHI4545 <singhabhitendrapratap@gmail.com>
2026-07-10 11:14:32 +0530

    Time: 2 ms (40.65%) | Memory: 45.4 MB (7.78%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -0,0 +1,35 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root==null) return 0;
+        int maxWidth = 0;
+        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
+        q.offer(new Pair<>(root,0));
+        while(!q.isEmpty()){
+            int size = q.size();
+            int minIdx = q.peek().getValue();
+            int first = 0,last = 0;
+            for(int i = 0;i<size;i++){
+                Pair<TreeNode,Integer> p = q.poll();
+                TreeNode node = p.getKey();
+                int currIdx = p.getValue() - minIdx;
+                if(i==0) first = currIdx;
+                if(i==size-1) last = currIdx;
+                if(node.left!=null) q.offer(new Pair<>(node.left,2*currIdx+1));
+                if(node.right!=null) q.offer(new Pair<>(node.right,2*currIdx+2));
+            }
+            maxWidth = Math.max(maxWidth,last-first+1);
+        }
+        return maxWidth;
+    }
+}
