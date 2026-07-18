46c675c664da2bd1dfb542a901f2f5deee559034
ITACHI4545 <singhabhitendrapratap@gmail.com>
2026-07-18 16:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (95.45%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,11 +1,9 @@
 class Solution {
-    private int dfs(int[] nums,int index,int currentXor){
-        if(index==nums.length) return currentXor;
-        int include = dfs(nums,index+1,currentXor^nums[index]);
-        int exclude = dfs(nums,index+1,currentXor);
-        return include + exclude;
-    }
     public int subsetXORSum(int[] nums) {
-        return dfs(nums,0,0);
+        int totalOr = 0;
+        for (int num : nums) {
+            totalOr |= num;
+        }
+        return totalOr << (nums.length - 1);
     }
 }
