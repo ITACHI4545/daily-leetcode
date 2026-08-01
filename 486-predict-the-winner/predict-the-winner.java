1c4ce95658f0a4a2bf7257bbfb8bf6d0427a4e88
ITACHI4545 <singhabhitendrapratap@gmail.com>
2026-08-01 08:56:30 +0530

    Time: 153 ms (5.13%) | Memory: 42.5 MB (89.58%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,11 +1,13 @@
 class Solution {
-    private int helper(int[] nums,int left,int right){
+    private int helper(int[] nums,int left,int right,Integer[][] memo){
         if(left>right) return 0;
-        int pickLeft = nums[left] - helper(nums,left+1,right);
-        int pickRight = nums[right] - helper(nums,left,right-1);
+        if(memo[left][right]!=null) return memo[left][right];
+        int pickLeft = nums[left] - helper(nums,left+1,right,memo);
+        int pickRight = nums[right] - helper(nums,left,right-1,memo);
         return Math.max(pickLeft,pickRight);
     }
     public boolean predictTheWinner(int[] nums) {
-       return helper(nums,0,nums.length-1)>=0;
+        Integer[][] memo = new Integer[nums.length][nums.length];
+       return helper(nums,0,nums.length-1,memo)>=0;
     }
 }
