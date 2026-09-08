316b00ea94226b3f9cf57223f336ce4d7ed00d6f
ITACHI4545 <singhabhitendrapratap@gmail.com>
2026-09-08 14:39:25 +0530

    Time: 7 ms (29.61%) | Memory: 46.8 MB (17.67%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,16 +1,24 @@
 class Solution {
+    int[][] dp;
+    private int solve(int i,int j,int[] piles){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int takei = piles[i] + Math.min(solve(i+2,j,piles),solve(i+1,j-1,piles));
+        int takej = piles[j] + Math.min(solve(i+1,j-1,piles),solve(i,j-2,piles));
+        return dp[i][j] = Math.max(takei,takej);
+    }
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = piles[i];
+        int sum = 0;
+        for(int num : piles){
+            sum += num;
         }
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                dp[i][j] = Math.max(piles[i] - dp[i + 1][j],piles[j] - dp[i][j - 1]);
-            }
+        dp = new int[n][n];
+        for(int[] row : dp){
+        Arrays.fill(row,-1);
         }
-        return dp[0][n - 1] > 0;
+        int aliceScore = solve(0,piles.length-1,piles);
+        if(aliceScore>sum/2) return true;
+        return false;
     }
 }
